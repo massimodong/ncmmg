6575ca341c2de6cebdd088a38f8e547acee7f05a
Massimo Dong <dongmassimo@gmail.com>
2020-05-15 16:05:56 +0800

    skip tests with UB

--- run.sh
+++ run.sh
@@ -6,8 +6,9 @@ PARSER=~/Lab/parser
 IRSIM=~/compilers-tests/irsim/build/irsim
 while [ $tot -lt 100000 ]; do
     ./generator > workdir/tmp.c
-    if gcc workdir/gcc.c -o workdir/a.out 2>/dev/null; then
-        if timeout 2 ./workdir/a.out < 0s.txt > workdir/gcc_out; then
+
+    if gcc workdir/gcc.c -o workdir/a.out -fsanitize=undefined -fno-sanitize-recover=undefined 2>/dev/null; then
+        if ./workdir/a.out < 0s.txt > workdir/gcc_out 2>/dev/null; then
             $PARSER workdir/tmp.c workdir/a.ir;
             $IRSIM workdir/a.ir < 0s.txt > workdir/cmm_out 2>/dev/null;
             if diff workdir/gcc_out workdir/cmm_out 2>/dev/null;then
